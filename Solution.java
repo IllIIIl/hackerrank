1d2f4ce7df36779a84c1197b193cf9de883ee93e
Zac Childress (oz_) <91855780+OZsoft-zac@users.noreply.github.com>
2021-11-01 18:34:34 -0500

    Output formatting practice on HackerRank (printf)

--- Solution.java
+++ Solution.java
@@ -1,23 +1,16 @@
-import java.io.*;
-import java.math.*;
-import java.security.*;
-import java.text.*;
-import java.util.*;
-import java.util.concurrent.*;
-import java.util.regex.*;
-
-
+import java.util.Scanner;
 
 public class Solution {
-    public static void main(String[] args) throws IOException {
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
 
-        int n = Integer.parseInt(bufferedReader.readLine().trim());
+    public static void main(String[] args) {
+            Scanner sc=new Scanner(System.in);
+            System.out.println("================================");
+            for(int i=0;i<3;i++){
+                String s1=sc.next();
+                int x=sc.nextInt();
+                System.out.printf("%-14s %03d%n", s1, x);
+            }
+            System.out.println("================================");
 
-        bufferedReader.close();
-        
-        for(int i = 1; i <= 10; i++){
-            System.out.println(n + " x " + i + " = " + (n*i));
-        }
     }
 }
